f749e98e512f0ee0075b340739d8f5af8f1c17fe
aleksejkosenko352-stack <aleksejkosenko352@gmail.com>
2025-11-28 17:30:34 +0200

    Create ProgramCodeModified.js

---
+++ .github/workflows/ProgramCodeModified.js
@@ -0,0 +1,11 @@
+// ProgramCode.js
+// Модуль для виведення назви програмного продукту SecureRest-SocialGuard
+
+const productName = "SecureRest-SocialGuard";
+
+function printProductName() {
+  console.log(`Назва програмного продукту: ${productName}`);
+}
+
+// Викликаємо функцію для виведення повідомлення у термінал
+printProductName();
